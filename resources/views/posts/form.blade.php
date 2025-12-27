eb9f3f1472c1f295dbdede4770c8b43643cfd645
ChanMyaeOo-Dev <chanmyaeoodev@gmail.com>
2025-12-27 15:45:17 +0630

    Ctrl + S Command Add

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -6,12 +6,7 @@
         <div class="flex items-center gap-2">
             <button id="full_screen_btn_update" type="button" form="data_form"
                 class="hidden items-center gap-2 px-3 py-2 text-sm font-medium text-white border border-rose-600 bg-rose-500 hover:bg-rose-600 rounded-md duration-300 cursor-pointer hover:-translate-y-1">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
-                    <path fill-rule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                        clip-rule="evenodd" />
-                </svg>
-                Update
+                Save
             </button>
             <button id="btn_post_body_zoom" type="button" class="btn_sm">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -97,6 +92,15 @@
 
 @push('script')
     <script>
+        document.addEventListener('keydown', function(e) {
+            const isCtrlOrCmd = e.ctrlKey || e.metaKey;
+
+            if (isCtrlOrCmd && e.key.toLowerCase() === 's') {
+                e.preventDefault();
+                // alert('CTRL + S detected');
+                document.getElementById('full_screen_btn_update')?.click();
+            }
+        });
         full_screen_btn_update.addEventListener('click', async () => {
             const form = document.getElementById('data_form');
             const button = document.getElementById('full_screen_btn_update');
@@ -136,7 +140,7 @@
                 }
             } finally {
                 button.disabled = false;
-                button.innerText = 'Update';
+                button.innerText = 'Save';
             }
         });
     </script>
